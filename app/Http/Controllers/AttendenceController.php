09c4d933c5150787afed2d70c4653b644ada596b
Ulan  Abikhan <u.abikhan02@mail.ru>
2022-05-03 00:49:52 +0600

    api/enter

--- app/Http/Controllers/AttendenceController.php
+++ app/Http/Controllers/AttendenceController.php
@@ -16,8 +16,20 @@ class AttendenceController extends Controller
         $review->production=$request->production;
         $review->break=$request->break;
 
+        $time = strtotime($request->day);
+        $punch_in = strtotime($request->punch_in);
+        $punch_out = strtotime($request->punch_out);
+        $day = date('Y-m-d', $time);
 
+        $review->day=$day;
+        $review->punch_in=date('H:i', $punch_in);
+        $review->punch_out=date('H:i', $punch_out);
 
-        return ["Result"=>"Has been"];
+        $result = $review->save();
+        if ($result)
+            return ["Result"=>"Data has been saved"];
+
+        else
+            return ["Result"=>"Not works"];
     }
 }
