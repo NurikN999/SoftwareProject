541ca93aca9fe29068df7c680953438333333101
Nurmukhamed Nurkamal <nurmukhamednurkamal@Nurmukhameds-MacBook-Air.local>
2022-06-26 04:24:00 +0600

    changes_in_nav

--- resources/views/sidebar/dashboard.blade.php
+++ resources/views/sidebar/dashboard.blade.php
@@ -3,39 +3,10 @@
         <div class="sidebar-inner slimscroll">
             <div id="sidebar-menu" class="sidebar-menu">
                 <ul>
-                    <li class="menu-title">
-                        <span>Main</span>
-                    </li>
-                    <li class="submenu">
-                        <a href="#" class="noti-dot">
-                            <i class="la la-dashboard"></i>
-                            <span> Dashboard</span>
-                            <span class="menu-arrow"></span>
-                        </a>
-                        <ul style="display: none;">
-                            <li><a class="active" href="{{ route('em/dashboard') }}">Employee Dashboard</a></li>
-                        </ul>
-                    </li>
-                    @if (Auth::user()->role_name=='Admin')
-                        <li class="menu-title"> <span>Authentication</span> </li>
-                        <li class="submenu">
-                            <a href="#">
-                                <i class="la la-user-secret"></i>
-                                <span> User Controller</span>
-                                <span class="menu-arrow"></span>
-                            </a>
-                            <ul style="display: none;">
-                                <li><a href="{{ route('userManagement') }}">All User</a></li>
-                                <li><a href="{{ route('activity/log') }}">Activity Log</a></li>
-                                <li><a href="{{ route('activity/login/logout') }}">Activity User</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                    <li class="menu-title"> <span>Employees</span> </li>
                     <li class="submenu">
                         <a href="#">
-                            <i class="la la-user"></i>
-                            <span> Employees</span>
+                            <i class="la la-star"></i>
+                            <span> Achievements</span>
                             <span class="menu-arrow"></span>
                         </a>
                         <ul style="display: none;">
@@ -46,9 +17,8 @@
                             <li><a href="{{ route('attendance/employee/page') }}">Attendance (Employee)</a></li>
                         </ul>
                     </li>
-                    <li class="menu-title"> <span>HR</span> </li>
 
-                    <li class="submenu"> <a href="#"><i class="la la-pie-chart"></i>
+                    <li class="submenu"> <a href="#"><i class="la la-times-circle"></i>
                             <span> Reports </span> <span class="menu-arrow"></span></a>
                         <ul style="display: none;">
                             <li><a href="{{ route('form/leave/reports/page') }}"> Leave Report </a></li>
@@ -56,13 +26,6 @@
                         </ul>
                     </li>
 
-                    <li class="menu-title"> <span>Pages</span> </li>
-                    <li class="submenu"> <a href="#"><i class="la la-user"></i>
-                            <span> Profile </span> <span class="menu-arrow"></span></a>
-                        <ul style="display: none;">
-                            <li><a href="profile.html"> Employee Profile </a></li>
-                        </ul>
-                    </li>
                 </ul>
             </div>
         </div>
