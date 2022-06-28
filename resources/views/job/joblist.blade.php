16e1a72aba571c5d2c28d5ae15b32ac653c34d59
Nurmukhamed Nurkamal <nurmukhamednurkamal@Nurmukhameds-MacBook-Air.local>
2022-06-28 23:28:32 +0600

    job_list_change

--- resources/views/job/joblist.blade.php
+++ resources/views/job/joblist.blade.php
@@ -5,57 +5,46 @@
         <div class="header">
             <!-- Logo -->
             <div class="header-left">
-                <a href="{{ route('home') }}" class="logo">
-                    <img src="{{ URL::to('assets/img/logo.png') }}" width="40" height="40" alt="">
+                <a href="{{ route('em/dashboard') }}" class="logo">
+                    <img src="{{ URL::to('assets/img/logo.svg') }}" width="40" height="40" alt="">
                 </a>
             </div>
             <!-- /Logo -->
             <!-- Header Title -->
             <div class="page-title-box float-left">
-                <h3>Admin Dashboard</h3>
+                <h3>Air Astana</h3>
             </div>
             <!-- /Header Title -->
             <!-- Header Menu -->
             <ul class="nav user-menu">
                 <!-- Search -->
-                <li class="nav-item">
-                    <div class="top-nav-search">
-                        <a href="javascript:void(0);" class="responsive-search">
-                            <i class="fa fa-search"></i>
-                    </a>
-                        <form action="search.html">
-                            <input class="form-control" type="text" placeholder="Search here">
-                            <button class="btn" type="submit"><i class="fa fa-search"></i></button>
-                        </form>
-                    </div>
-                </li>
                 <!-- /Search -->
-            
+
                 <!-- Flag -->
-                <li class="nav-item dropdown has-arrow flag-nav">
-                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button">
-                        <img src="{{ URL::to('assets/img/flags/us.png') }}" alt="" height="20"> <span>English</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="{{ URL::to('assets/img/flags/us.png') }}" alt="" height="16"> English
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="{{ URL::to('assets/img/flags/kh.png') }}" alt="" height="16"> Khmer 
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="{{ URL::to('assets/img/flags/fr.png') }}" alt="" height="16"> French
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="{{ URL::to('assets/img/flags/es.png') }}" alt="" height="16"> Spanish
-                        </a>
-                        <a href="javascript:void(0);" class="dropdown-item">
-                            <img src="{{ URL::to('assets/img/flags/de.png') }}" alt="" height="16"> German
-                        </a>
-                    </div>
-                </li>
+{{--                <li class="nav-item dropdown has-arrow flag-nav">--}}
+{{--                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button">--}}
+{{--                        <img src="{{ URL::to('assets/img/flags/us.png') }}" alt="" height="20"> <span>English</span>--}}
+{{--                    </a>--}}
+{{--                    <div class="dropdown-menu dropdown-menu-right">--}}
+{{--                        <a href="javascript:void(0);" class="dropdown-item">--}}
+{{--                            <img src="{{ URL::to('assets/img/flags/us.png') }}" alt="" height="16"> English--}}
+{{--                        </a>--}}
+{{--                        <a href="javascript:void(0);" class="dropdown-item">--}}
+{{--                            <img src="{{ URL::to('assets/img/flags/kh.png') }}" alt="" height="16"> Khmer--}}
+{{--                        </a>--}}
+{{--                        <a href="javascript:void(0);" class="dropdown-item">--}}
+{{--                            <img src="{{ URL::to('assets/img/flags/fr.png') }}" alt="" height="16"> French--}}
+{{--                        </a>--}}
+{{--                        <a href="javascript:void(0);" class="dropdown-item">--}}
+{{--                            <img src="{{ URL::to('assets/img/flags/es.png') }}" alt="" height="16"> Spanish--}}
+{{--                        </a>--}}
+{{--                        <a href="javascript:void(0);" class="dropdown-item">--}}
+{{--                            <img src="{{ URL::to('assets/img/flags/de.png') }}" alt="" height="16"> German--}}
+{{--                        </a>--}}
+{{--                    </div>--}}
+{{--                </li>--}}
                 <!-- /Flag -->
-                
+
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </li>
@@ -85,7 +74,7 @@
                         <div class="col-sm-12">
                             <h3 class="page-title">Jobs</h3>
                             <ul class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('em/dashboard') }}">Dashboard</a></li>
                                 <li class="breadcrumb-item active">Jobs</li>
                             </ul>
                         </div>
