a6f444d7f601affad730a75a5b42dd1315cae0e4
Nurmukhamed Nurkamal <nurmukhamednurkamal@Nurmukhameds-MacBook-Air.local>
2022-07-02 22:07:06 +0600

    pointsemployee

--- resources/views/dashboard/emdashboard.blade.php
+++ resources/views/dashboard/emdashboard.blade.php
@@ -36,7 +36,7 @@
                         <ul style="display: none;">
                             <li><a href="{{ route('all/employee/card') }}">All Employees</a></li>
                             <li><a href="{{ route('form/holidays/new') }}">Holidays</a></li>
-                            <li><a href="{{route('form/pointsemployee')}}">Leaves (Employee)</a></li>
+                            <li><a href="{{route('form/pointsemployee')}}">Points of Employee</a></li>
                             <li><a href="{{ route('form/leavesettings/page') }}">Leave Settings</a></li>
                             <li><a href="{{ route('attendance/employee/page') }}">Attendance (Employee)</a></li>
                         </ul>
